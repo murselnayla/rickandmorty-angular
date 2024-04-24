5e4bcf46e1127f66a3a3ad303971cebd6fd1f7ed
Mursel Elibol <mursel.elibol@naylalabs.com>
2024-04-24 21:29:19 +0300

    fix: layout

--- src/app/layouts/home-layout/components/sidebar/sidebar.component.ts
+++ src/app/layouts/home-layout/components/sidebar/sidebar.component.ts
@@ -2,6 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgForOf } from '@angular/common';
 
+interface INavItems {
+	icon: string;
+	label: string;
+	link: string;
+}
+
 @Component({
 	selector: 'app-sidebar',
 	standalone: true,
@@ -11,7 +17,7 @@ import { NgForOf } from '@angular/common';
 	imports: [RouterLink, NgForOf, RouterLinkActive],
 })
 export class SidebarComponent {
-	public NAV_ITEMS: any = [
+	public NAV_ITEMS: INavItems[] = [
 		{
 			icon: 'pi pi-home',
 			label: 'Home',
