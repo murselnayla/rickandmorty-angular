6a299b4d9a0b6613e9ad69c0d47731b27dafd84f
Mursel Elibol <mursel.elibol@naylalabs.com>
2024-04-25 21:46:20 +0300

    feat: add pagination and filter in characters page

--- src/app/pages/characters-page/characters-page.component.ts
+++ src/app/pages/characters-page/characters-page.component.ts
@@ -2,7 +2,25 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@
 import { CharacterCardComponent } from '@components/character-card/character-card.component';
 import { CharacterService } from '@app/services';
 import { ICharacterRes, PaginationRes } from '@app/core/dtos';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { CHARACTER_STATUS, CHARACTER_GENDER } from '@app/core/constants';
+import { IDropdownItem } from '@app/core/interfaces';
+import { ButtonModule } from 'primeng/button';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { MessageService } from 'primeng/api';
+import { toastHttpError } from '@app/core/utils';
+
+interface IQueryParam {
+	page?: number;
+	name?: string;
+	status?: string;
+	species?: string;
+	type?: string;
+	gender?: string;
+}
 
 @Component({
 	selector: 'app-characters-page',
@@ -10,27 +28,76 @@ import { NgForOf } from '@angular/common';
 	templateUrl: './characters-page.component.html',
 	styleUrls: ['./characters-page.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [CharacterCardComponent, NgForOf],
+	imports: [
+		CharacterCardComponent,
+		NgForOf,
+		PaginatorModule,
+		CardModule,
+		InputTextModule,
+		ButtonModule,
+		ProgressSpinnerModule,
+		NgIf,
+	],
 })
 export class CharactersPageComponent implements OnInit {
+	public isLoading: boolean = false;
 	public characters: ICharacterRes[] = [];
+	public CHARACTER_STATUS: IDropdownItem[] = CHARACTER_STATUS;
+	public CHARACTER_GENDER: IDropdownItem[] = CHARACTER_GENDER;
+	public totalPages = 0;
+	public query!: IQueryParam;
 
 	constructor(
 		private characterService: CharacterService,
+		private messageService: MessageService,
 		private cdRef: ChangeDetectorRef,
-	) {}
-
-	ngOnInit() {
-		this.getCharacters();
+	) {
+		this.setQuery();
 	}
 
-	public getCharacters() {
-		this.characterService.getAll().subscribe({
+	ngOnInit() {
+		this.getCharacters(this.query);
+	}
+
+	private setQuery() {
+		this.query = {
+			page: 1,
+			name: '',
+			status: '',
+			species: '',
+			type: '',
+			gender: '',
+		};
+	}
+
+	private getCharacters(query?: any) {
+		this.isLoading = true;
+		this.characterService.getAll(query).subscribe({
 			next: (data: PaginationRes<ICharacterRes>) => {
 				this.characters = data.results;
+				this.totalPages = data.info.pages;
+				this.isLoading = false;
 				this.cdRef.detectChanges();
 			},
-			error: (err) => console.error(err),
+			error: ({ error }) => {
+				toastHttpError(error, this.messageService);
+				this.isLoading = false;
+				this.cdRef.detectChanges();
+			},
 		});
+	}
+
+	public onChangePageQuery(event: any, query: any) {
+		query.page = event.page + 1;
+		this.getCharacters(query);
+	}
+
+	public onClickSearch(query: any) {
+		this.getCharacters(query);
+	}
+
+	public onClickReset() {
+		this.setQuery();
+		this.getCharacters();
 	}
 }
--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { getEndpoint } from '@source/environments/environment';
 import { catchError, Observable, retry, throwError } from 'rxjs';
 import { ICharacterRes, PaginationRes } from '@app/core/dtos';
+import { removeEmptyQueryParams } from '@app/core/utils';
 
 @Injectable({
 	providedIn: 'root',
@@ -11,21 +12,19 @@ export class CharacterService {
 	constructor(private readonly http: HttpClient) {}
 
 	public getAll(params?: any): Observable<PaginationRes<ICharacterRes>> {
-		const myParams = {
-			page: 1,
-		};
-
-		return this.http.get<PaginationRes<ICharacterRes>>(getEndpoint('character'), { params: myParams }).pipe(
-			retry(1),
-			catchError((error: HttpErrorResponse) => throwError(() => new Error(error.error))),
-		);
+		return this.http
+			.get<PaginationRes<ICharacterRes>>(getEndpoint('character'), { params: removeEmptyQueryParams(params) })
+			.pipe(
+				retry(1),
+				catchError((error: HttpErrorResponse) => throwError(() => error.error)),
+			);
 	}
 
 	public get(id: number): Observable<ICharacterRes> {
 		const path = `character/${id}`;
 		return this.http.get<ICharacterRes>(getEndpoint(path)).pipe(
 			retry(1),
-			catchError((error: HttpErrorResponse) => throwError(() => new Error(error.error))),
+			catchError((error: HttpErrorResponse) => throwError(() => error.error)),
 		);
 	}
 }
