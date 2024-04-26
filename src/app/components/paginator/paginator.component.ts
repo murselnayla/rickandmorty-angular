dfe8207e2c69bb8cc24e0738fbb916935d651ef8
Mursel Elibol <mursel.elibol@naylalabs.com>
2024-04-26 14:23:52 +0300

    feat: add paginator component

---
+++ src/app/components/paginator/paginator.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { PaginatorModule } from 'primeng/paginator';
+
+@Component({
+	selector: 'app-paginator',
+	standalone: true,
+	templateUrl: './paginator.component.html',
+	styleUrls: ['./paginator.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [PaginatorModule],
+})
+export class PaginatorComponent {
+	@Input() totalPages!: number;
+	@Output() changePage = new EventEmitter<number>();
+}
