ea60a8114acab0d9f1f9fcf9cc08580d0fc09077
Mursel Elibol <mursel.elibol@naylalabs.com>
2024-04-26 11:06:45 +0300

    config: add primary color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{html,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					50: '#f2f8ff',
+					100: '#c2dfff',
+					200: '#91c6ff',
+					300: '#61adff',
+					400: '#3094ff',
+					500: '#007bff',
+					600: '#0069d9',
+					700: '#0056b3',
+					800: '#00448c',
+					900: '#003166',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
